eef7e1844cae63dba24c63ce6d20d77d8467f1fc
Jula <julieta.dubra@fing.edu.uy>
2021-09-01 00:45:04 -0300

    husky fix for ts files

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,16 @@
-import {Request,Response,Application} from 'express';
+import { Request, Response } from "express";
 
-const {parseExcel} = require('./Controllers/ExcelController')
-const { Router } = require('express');
+const { Router } = require("express");
+const { parseExcel } = require("./Controllers/ExcelController.ts");
+
 const router = Router();
 
+router.get("/", (req:Request, res:Response):void => {
+  res.send("Hey! This is REPP API, you can go to /api-docs to learn more!");
+});
 
-router.get('/',(req:Request, res:Response):void => {
-    res.send("Hey! This is REPP API, you can go to /api-docs to learn more!")
-  })
-
-//Che esto es para ejemplo de como usar swagger, hay que arreglarlo
-//TODO
+// Che esto es para ejemplo de como usar swagger, hay que arreglarlo
+// TODO
 /**
  * @swagger
  * /excelParser:
@@ -29,7 +29,7 @@ router.get('/',(req:Request, res:Response):void => {
  *                          -   password
  *                      properties:
  *                          excel:
- *                              type: string  
+ *                              type: string
  *      responses:
  *          '200':
  *              description: returns the parsed JSON of the excel file provided
@@ -41,6 +41,6 @@ router.get('/',(req:Request, res:Response):void => {
  *                              excelParsed:
  *                                  type: string
  */
-router.post('/excelParser', parseExcel)
+router.post("/excelParser", parseExcel);
 
-module.exports =  router
+module.exports = router;
