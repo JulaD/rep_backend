e39bc1577452ef6ea27d88dd71e935b4a1542379
Nicolás San Martín <nicolassanmar@gmail.com>
2021-09-01 00:28:21 -0300

    eslint aber ke tal

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
-import express, {Application} from 'express';
+import express, { Application } from "express";
 
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-
-const app:Application = express();
+const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
 // swagger init
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'REPP Rest API',
-            description: '',
-            servers: ['http://localhost:3000']
-        }
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REPP Rest API",
+      description: "",
+      servers: ["http://localhost:3000"],
     },
-    apis: ['src/routes.ts']
-  }
-const swaggerDocs = swaggerJsDoc(swaggerOptions)
-
+  },
+  apis: ["src/routes.ts"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 // middlewares
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-function q(){
-    return undefined
+function q() {
+  return undefined;
 }
 
-app.use(require('./routes.ts'))
+app.use(require("./routes.ts"));
 
-app.listen(PORT, ():void => {
-console.log(`REPP Backend running here 👉 https://localhost:${PORT}`);
+app.listen(PORT, (): void => {
+  console.log(`REPP Backend running here 👉 https://localhost:${PORT}`);
 });
