daaa56795ed115a54c39afb6fe39cdf28432b0f1
Renzo Beux <renzobeux@gmail.com>
2021-10-24 22:16:23 -0300

    hotfix

--- src/Middlewares/authChecker.ts
+++ src/Middlewares/authChecker.ts
@@ -2,15 +2,15 @@ import { Response, NextFunction } from 'express';
 import { validate } from '../Services/UserAPI';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const authChecker = (req: any, res: Response, next: NextFunction): void => {
-  const token = req.headers.authorization;
-  const userId = validate(token);
-  if (userId === -1) {
+const authChecker = async (req: any, res: Response, next: NextFunction): void => {
+  try {
+    const token = req.headers.authorization;
+    const userId = await validate(token);
+    req.user_id = userId;
+    next();
+  } catch (error) {
     res.status(401).send({ message: 'auth failed' });
-    return;
   }
-  req.user_id = userId;
-  next();
 };
 
 export default authChecker;
