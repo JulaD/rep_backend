9c6042c93cfb224dcbd67cabb72b59b5e2ada14b
Agustin <agusr1999@gmail.com>
2021-11-20 19:33:07 -0300

    adds more timeouts to avoid async issues in unit tests

--- __test__/CalculatorService.test.ts
+++ __test__/CalculatorService.test.ts
@@ -7,98 +7,102 @@ import ParameterDataBaseLoader from '../src/Loaders/ParameterDataBaseLoader';
 describe('Verificar si funciona calculo del RE para menores de 1 año', () => {
   it('init database', async () => {
     await ParameterDataBaseLoader.initParameterDataBase();
-    setTimeout(() => {}, 100);
+    setTimeout(() => {}, 50);
   });
   it('Mujeres', async () => {
-    expect(await CalculatorService.calculateEnergeticRequirement([{
-      age: age.m3,
-      sex: sex.Female,
-      medianWeight: 6,
-      population: 9,
-    }],
-    {
-      minorPAL: {
-        lowPALPrevalence: 0,
-        moderatePALPrevalence: 100,
-        intensePALPrevalence: 0,
-      },
-      adultPAL: {
-        urbanPercentage: 94.7,
-        activeUrbanPAL: 10,
-        lowUrbanPAL: 90,
-        ruralPercentage: 5.3,
-        activeRuralPAL: 50,
-        lowRuralPAL: 50,
-      },
-      maternity18To29: {
-        countryBirthRate: 14,
-        countryWomenInAgeGroup: 315790,
-        countryPopulation: 3453691,
-      },
-      maternity30To59: undefined,
-    })).toStrictEqual({
-      groupsRequirements: [{
-        group: {
-          age: age.m3 as string,
-          sex: sex.Female as string,
-          medianWeight: 6,
-          population: 9,
-        },
-        perCapita: 473,
-        total: 4255,
+    setTimeout(async () => {
+      expect(await CalculatorService.calculateEnergeticRequirement([{
+        age: age.m3,
+        sex: sex.Female,
+        medianWeight: 6,
+        population: 9,
       }],
-      totalRequirement: {
-        perCapita: 473,
-        total: 4255,
-        totalPopulation: 9,
-      },
-    });
+      {
+        minorPAL: {
+          lowPALPrevalence: 0,
+          moderatePALPrevalence: 100,
+          intensePALPrevalence: 0,
+        },
+        adultPAL: {
+          urbanPercentage: 94.7,
+          activeUrbanPAL: 10,
+          lowUrbanPAL: 90,
+          ruralPercentage: 5.3,
+          activeRuralPAL: 50,
+          lowRuralPAL: 50,
+        },
+        maternity18To29: {
+          countryBirthRate: 14,
+          countryWomenInAgeGroup: 315790,
+          countryPopulation: 3453691,
+        },
+        maternity30To59: undefined,
+      })).toStrictEqual({
+        groupsRequirements: [{
+          group: {
+            age: age.m3 as string,
+            sex: sex.Female as string,
+            medianWeight: 6,
+            population: 9,
+          },
+          perCapita: 473,
+          total: 4255,
+        }],
+        totalRequirement: {
+          perCapita: 473,
+          total: 4255,
+          totalPopulation: 9,
+        },
+      });
+    }, 100);
   });
   it('Varones', async () => {
-    expect(await CalculatorService.calculateEnergeticRequirement([{
-      age: age.m11,
-      sex: sex.Male,
-      medianWeight: 12,
-      population: 7,
-    }],
-    {
-      minorPAL: {
-        lowPALPrevalence: 0,
-        moderatePALPrevalence: 100,
-        intensePALPrevalence: 0,
-      },
-      adultPAL: {
-        urbanPercentage: 94.7,
-        activeUrbanPAL: 10,
-        lowUrbanPAL: 90,
-        ruralPercentage: 5.3,
-        activeRuralPAL: 50,
-        lowRuralPAL: 50,
-      },
-      maternity18To29: {
-        countryBirthRate: 14,
-        countryWomenInAgeGroup: 315790,
-        countryPopulation: 3453691,
-      },
-      maternity30To59: undefined,
-    })).toStrictEqual({
-      groupsRequirements: [{
-        group: {
-          age: age.m11 as string,
-          sex: sex.Male as string,
-          medianWeight: 12,
-          population: 7,
-        },
-        perCapita: 986,
-        total: 6901,
+    setTimeout(async () => {
+      expect(await CalculatorService.calculateEnergeticRequirement([{
+        age: age.m11,
+        sex: sex.Male,
+        medianWeight: 12,
+        population: 7,
       }],
-      totalRequirement: {
-        perCapita: 986,
-        total: 6901,
-        totalPopulation: 7,
-      },
+      {
+        minorPAL: {
+          lowPALPrevalence: 0,
+          moderatePALPrevalence: 100,
+          intensePALPrevalence: 0,
+        },
+        adultPAL: {
+          urbanPercentage: 94.7,
+          activeUrbanPAL: 10,
+          lowUrbanPAL: 90,
+          ruralPercentage: 5.3,
+          activeRuralPAL: 50,
+          lowRuralPAL: 50,
+        },
+        maternity18To29: {
+          countryBirthRate: 14,
+          countryWomenInAgeGroup: 315790,
+          countryPopulation: 3453691,
+        },
+        maternity30To59: undefined,
+      })).toStrictEqual({
+        groupsRequirements: [{
+          group: {
+            age: age.m11 as string,
+            sex: sex.Male as string,
+            medianWeight: 12,
+            population: 7,
+          },
+          perCapita: 986,
+          total: 6901,
+        }],
+        totalRequirement: {
+          perCapita: 986,
+          total: 6901,
+          totalPopulation: 7,
+        },
+      });
     });
-  });
+  }, 100);
 });
 
 describe('Verificar si funciona calculo del RE para personas de 1 a 5 años', () => {
